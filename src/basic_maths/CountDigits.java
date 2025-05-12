53bcba038489ade74ab896c876c32ba8bb41dd5c
Davood Bhat <davoodbhat@Mac.lan>
2025-05-12 15:14:31 +0530

    Count Digits

--- src/basic_maths/CountDigits.java
+++ src/basic_maths/CountDigits.java
@@ -1,2 +1,16 @@
-package basic_maths;public class CountDigits {
+package basic_maths;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        int number = 1;
+        System.out.println("Number of digits in " + number + ": " + countDigits(number));
+    }
+    private static int countDigits(int number) {
+        int count =0;
+        while (number > 0) {
+            number = number / 10; // Remove the last digit
+            count++;
+        }
+        return count;
+    }
 }
